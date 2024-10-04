e016137ebbac904ece0060a2f452a0d42018632d
Ysf-08 <youssefboufounas25@gmail.com>
2024-10-04 15:38:55 +0100

    Premier commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function openGithub(url) {
+    window.open(url, '_blank');
+}
+// script.js
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
